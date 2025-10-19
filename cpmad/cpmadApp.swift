727757417da36b175db3c79ac517a09402a2952d
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-10-19 20:10:20 +0200

    Initial Commit

---
+++ cpmad/cpmadApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpmadApp.swift
+//  cpmad
+//
+//  Created by Karolina Nowacka on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct cpmadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
